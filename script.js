c9f7d5cb650dce5472445e2b9fd7f757d60a9840
joaovprimo <fulano@email.com>
2022-06-11 19:19:07 -0300

    atualizado3

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-
-function selecionarBorda(elemento){
-const opcaoClicada = document.querySelector(".selecionado")
+function opcaoPrato(elemento){
+const opcaoClicada = document.querySelector(".pratos .selecionado");
 if (opcaoClicada !== null){
     opcaoClicada.classList.remove("selecionado");
     }
 elemento.classList.add("selecionado");
-console.log (opcaoClicada);
 }
 
+function opcaoBebida(elemento){
+    const opcaoClicada = document.querySelector(".bebidas .selecionado");
+    if (opcaoClicada !== null){
+        opcaoClicada.classList.remove("selecionado");
+        }
+    elemento.classList.add("selecionado");
+    }
 
+    function opcaoDoces(elemento){
+        const opcaoClicada = document.querySelector(".doces .selecionado");
+        if (opcaoClicada !== null){
+            opcaoClicada.classList.remove("selecionado");
+            }
+        elemento.classList.add("selecionado");
+        }
